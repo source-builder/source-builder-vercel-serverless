98de3887a0d51e27cdd13aa47043d5da3d5dbbf9
mythcsj <mythcsj@163.com>
2023-08-11 09:40:40 +0800

    [fix] modify code

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default (request: VercelRequest, response: VercelResponse) => {
+    response.status(200).send(JSON.stringify(request.query))
+}
